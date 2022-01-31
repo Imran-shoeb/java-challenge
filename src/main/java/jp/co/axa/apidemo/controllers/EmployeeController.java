a7a36c04063459d9169c516dcf878b593ab7867e
Imranul Islam <shoeb1219@gmail.com>
2022-01-31 13:06:20 +0900

    added ApiOperation annotation in the EmployeeController

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -5,10 +5,14 @@ import jp.co.axa.apidemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(value="employeeInfo", description="Operations for the employee information")
 public class EmployeeController {
 
     @Autowired
@@ -17,30 +21,35 @@ public class EmployeeController {
     public void setEmployeeService(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-
+    
+    @ApiOperation(value = "View a list of employees", response = Iterable.class)
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         List<Employee> employees = employeeService.retrieveEmployees();
         return employees;
     }
 
+    @ApiOperation(value = "Search an employee with an ID",response = Employee.class)
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
         return employeeService.getEmployee(employeeId);
     }
 
+    @ApiOperation(value = "Add an employee")
     @PostMapping("/employees")
     public void saveEmployee(Employee employee){
         employeeService.saveEmployee(employee);
         System.out.println("Employee Saved Successfully");
     }
 
+    @ApiOperation(value = "Delete an employee")
     @DeleteMapping("/employees/{employeeId}")
     public void deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
         employeeService.deleteEmployee(employeeId);
         System.out.println("Employee Deleted Successfully");
     }
 
+    @ApiOperation(value = "Update an employee with ID")
     @PutMapping("/employees/{employeeId}")
     public void updateEmployee(@RequestBody Employee employee,
                                @PathVariable(name="employeeId")Long employeeId){
